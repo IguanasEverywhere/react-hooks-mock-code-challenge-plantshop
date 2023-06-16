c1f389066ed59704d1cd3366c44bba0909809781
Scott Schwab <scottschwab86@gmail.com>
2023-06-16 11:54:52 -0500

    Complete core deliverables

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onNewPlantSubmit }) {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    image: '',
+    price: ''
+  })
+
+  function handleFormChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onNewPlantSubmit(formData);
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleFormChange} type="text" name="name" placeholder="Plant name" value={formData.name} />
+        <input onChange={handleFormChange} type="text" name="image" placeholder="Image URL" value={formData.image} />
+        <input onChange={handleFormChange} type="number" name="price" step="0.01" placeholder="Price" value={formData.price} />
         <button type="submit">Add Plant</button>
       </form>
     </div>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [allPlants, setAllPlants] = useState([]);
+  const [plantsToDisplay, setPlantsToDisplay] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(r => r.json())
+      .then(plants => {
+        setAllPlants(plants);
+        setPlantsToDisplay(plants)
+      })
+  }, [])
+
+  function onNewPlantSubmit(formData) {
+    fetch('http://localhost:6001/plants', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(r => r.json())
+    .then(newPlant => {
+       setAllPlants([...allPlants, newPlant]);
+       setPlantsToDisplay([...plantsToDisplay, newPlant]);
+    })
+  }
+
+  function searchForPlants(searchValue) {
+    const filteredPlants = allPlants.filter((plant) => plant.name.toLowerCase().includes(searchValue.toLowerCase()))
+    setPlantsToDisplay(filteredPlants)
+  }
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onNewPlantSubmit={onNewPlantSubmit}/>
+      <Search searchForPlants={searchForPlants}/>
+      <PlantList plantsToDisplay={plantsToDisplay} />
     </main>
   );
 }
